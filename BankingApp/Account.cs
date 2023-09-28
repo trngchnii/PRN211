24913a5690b05ef695953f974bb78eb5262647ec
unknown <ni.trnh59@gmail.com>
2023-09-28 10:56:13 +0700

    lab4

---
+++ BankingApp/Account.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp
+{
+    using System;
+
+    public class Account
+    {
+        private decimal balance;
+
+        public delegate void BalanceChangedEventHandler(decimal newBalance);
+
+        public event BalanceChangedEventHandler BalanceChanged;
+
+        public decimal Balance
+        {
+            get { return balance; }
+            set
+            {
+                balance = value;
+                OnBalanceChanged(balance);
+            }
+        }
+
+        protected virtual void OnBalanceChanged(decimal newBalance)
+        {
+            if (BalanceChanged != null)
+            {
+                BalanceChanged(newBalance);
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Account account = new Account();
+
+            account.BalanceChanged += Account_BalanceChanged;
+
+            account.Balance += 1000;
+
+            account.Balance -= 50;
+        }
+
+        private static void Account_BalanceChanged(decimal newBalance)
+        {
+            Console.WriteLine($"New account balance: {newBalance}");
+        }
+    }
+}
---
+++ DelegateGenerics/Calculator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateGenerics
+{
+
+    public delegate T MathOperation<T>(T a, T b);
+
+    public class Calculator
+    {
+        public static T Add<T>(T a, T b)
+        {
+            dynamic num1 = a;
+            dynamic num2 = b;
+            return num1 + num2;
+        }
+
+        public static T Subtract<T>(T a, T b)
+        {
+            dynamic num1 = a;
+            dynamic num2 = b;
+            return num1 - num2;
+        }
+
+        public static T Multiply<T>(T a, T b)
+        {
+            dynamic num1 = a;
+            dynamic num2 = b;
+            return num1 * num2;
+        }
+
+        public static T Divide<T>(T a, T b)
+        {
+            dynamic num1 = a;
+            dynamic num2 = b;
+            return num1 / num2;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            // Declare delegates
+            MathOperation<int> addDelegate = Calculator.Add;
+            MathOperation<int> subtractDelegate = Calculator.Subtract;
+            MathOperation<int> multiplyDelegate = Calculator.Multiply;
+            MathOperation<int> divideDelegate = Calculator.Divide;
+
+            // Perform calculations
+            int a = 10, b = 5;
+            int result;
+
+            result = addDelegate(a, b);
+            Console.WriteLine($"Addition: {result}");
+
+            result = subtractDelegate(a, b);
+            Console.WriteLine($"Subtraction: {result}");
+
+            result = multiplyDelegate(a, b);
+            Console.WriteLine($"Multiplication: {result}");
+
+            result = divideDelegate(a, b);
+            Console.WriteLine($"Division: {result}");
+        }
+    }
+}
