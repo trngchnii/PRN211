fe94fa7897fb1b457199108d512cb3a9ca7ce35f
unknown <ni.trnh59@gmail.com>
2023-09-28 11:15:36 +0700

    Revert "add project files"
    This reverts commit ab4f01466a2fe1c4d39d5c48855c5bd667459d1a.

---
+++ Slot7/Program1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slot7
+{
+    internal class Program1
+    {
+        static int Add(int numOne, int numTwo) => numOne + numTwo;
+        static int Subtract(int numOne, int numTwo) => numOne - numTwo;
+        static void Main(string[] args)
+        {
+            int n1 = 25;
+            int n2 = 15;
+            int resurt;
+            MyDelegate obj1 = new MyDelegate(Add);
+            resurt = obj1(n1, n2);
+            Console.WriteLine($"{n1} + {n1} = {resurt}");
+            MyDelegate obj2 = new MyDelegate(Subtract);
+            resurt = obj2.Invoke(n1, n2);
+            Console.WriteLine($"{n1} - {n1} = {resurt}");
+            Console.ReadLine();
+        }
+    }
+}
